c5dab0a4350ff76f01fd41b9812dbb79c8b166bf
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2026-02-14 07:20:40 +0300

    use hoodi testnet as default network

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { createTheme, useColorScheme } from "@mui/material/styles";
-import { type AppKitNetwork, arbitrum, avalanche, base, bsc, hoodi, mainnet } from "@reown/appkit/networks";
+import { type AppKitNetwork, hoodi } from "@reown/appkit/networks";
 import { createAppKit } from "@reown/appkit/react";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -46,8 +46,8 @@ const NAVIGATION: Navigation = [
 const queryClient = new QueryClient();
 
 const projectId = import.meta.env.VITE_REOWN_PROJECT_ID as string;
-const defaultNetwork = mainnet;
-const networks = [defaultNetwork, bsc, base, arbitrum, avalanche, hoodi] as [AppKitNetwork, ...AppKitNetwork[]];
+const defaultNetwork = hoodi;
+const networks = [defaultNetwork] as [AppKitNetwork, ...AppKitNetwork[]];
 const wagmiAdapter = new WagmiAdapter({ projectId, networks });
 
 createAppKit({
