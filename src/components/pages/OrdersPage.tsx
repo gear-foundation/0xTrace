242bd5ae310ef8e428d6109729fc84f0cc88cef5
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2026-02-14 06:13:16 +0300

    add cryptography

--- src/components/pages/OrdersPage.tsx
+++ src/components/pages/OrdersPage.tsx
@@ -1,5 +1,25 @@
 import Typography from "@mui/material/Typography";
+import { checkStealthAddress, computeStealthKey, generateStealthAddress } from "@/cryptography/StealthAddresses";
 
 export default function OrdersPage() {
+  const { stealthAddress, viewTag, ephemeralPublicKey } = generateStealthAddress(
+    "st:eth:0x034f6340cfdd930a6f54e730188e3071d150877fa664945fb6f120c18b56ce1c090201b0906e61ad4fcb2b91129d75723a1c6cd03d56b52a6a78a155292f0cf558e7",
+  );
+  const ret = checkStealthAddress(
+    stealthAddress,
+    ephemeralPublicKey,
+    viewTag,
+    "0xe106b40d369f5c94f5dd2a13d9131585121002ed9e313d2dc9e49ff534c50bd1",
+    "0x034f6340cfdd930a6f54e730188e3071d150877fa664945fb6f120c18b56ce1c09",
+  );
+  console.log(ret);
+  const stealthPrivateKey = computeStealthKey(
+    stealthAddress,
+    ephemeralPublicKey,
+    "0xe106b40d369f5c94f5dd2a13d9131585121002ed9e313d2dc9e49ff534c50bd1",
+    "0xa4ddf31f7f32ba696f14ce50ecf3f21e3e100e83bdf47966e7b07468e9500b6e",
+  );
+  console.log(stealthPrivateKey);
+
   return <Typography>Welcome to the Toolpad orders!</Typography>;
 }
--- src/cryptography/StealthAddresses.ts
+++ src/cryptography/StealthAddresses.ts
@@ -1,6 +1,6 @@
 import { secp256k1 } from "@noble/curves/secp256k1";
-import { type Hex, toHex } from "viem";
-import { english, generateMnemonic, mnemonicToAccount } from "viem/accounts";
+import { type Address, bytesToBigInt, checksumAddress, fromHex, type Hex, keccak256, toBytes, toHex } from "viem";
+import { english, generateMnemonic, mnemonicToAccount, privateKeyToAddress } from "viem/accounts";
 
 export function generateMnemonicExtended() {
   const mnemonic = generateMnemonic(english);
@@ -34,4 +34,106 @@ export function generateNewStealthAddress(chain: string = "eth") {
     stealthAddressHex,
     stealthAddress,
   };
+}
+
+export function generateStealthAddress(stealthMetaAddress: string) {
+  const regExpMatch = stealthMetaAddress.match(/^st:([^:]+):0x([0-9a-fA-F]+)$/);
+  if (!regExpMatch) {
+    throw new Error("Invalid stealth address format, expected: st:<chain>:0x<hex>");
+  }
+
+  const chain = regExpMatch[1];
+  if (chain !== "eth") {
+    throw new Error("Unsupported chain");
+  }
+
+  const stealthMetaAddressHex = regExpMatch[2];
+  const EXPECTED_HEX_LEN = 66 * 2;
+  if (stealthMetaAddressHex.length !== EXPECTED_HEX_LEN) {
+    throw new Error("Invalid stealth hex length");
+  }
+
+  const spendPublicKeyHex = `0x${stealthMetaAddressHex.slice(0, 66)}`;
+  const viewPublicKeyHex = `0x${stealthMetaAddressHex.slice(66)}`;
+
+  const spendPublicKeyBuffer = Buffer.from(spendPublicKeyHex.slice(2), "hex");
+  const viewPublicKeyBuffer = Buffer.from(viewPublicKeyHex.slice(2), "hex");
+
+  const spendPublicKey = secp256k1.Point.fromBytes(new Uint8Array(spendPublicKeyBuffer));
+  const viewPublicKey = secp256k1.Point.fromBytes(new Uint8Array(viewPublicKeyBuffer));
+
+  //const ephemeralPrivateKey = secp256k1.utils.randomPrivateKey();
+  const ephemeralPrivateKey = fromHex("0x5966e42b1db433cdeca15434ff2fa8b8bea7a4d9aa95a41cffcd0fd54cb020cf", {
+    size: 32,
+    to: "bytes",
+  });
+  const ephemeralPublicKey = toHex(secp256k1.Point.fromPrivateKey(ephemeralPrivateKey).toBytes());
+
+  const sharedSecretPoint = viewPublicKey.multiply(bytesToBigInt(ephemeralPrivateKey)).toAffine();
+  const sharedSecretX = sharedSecretPoint.x;
+  const sharedSecretXHashed = keccak256(toBytes(sharedSecretX), "bytes");
+
+  const viewTag = toHex(sharedSecretXHashed.slice(0, 1));
+
+  const sharedSecretXHashedPublicKey = secp256k1.Point.fromPrivateKey(sharedSecretXHashed);
+
+  const stealthPublicKey = spendPublicKey.add(sharedSecretXHashedPublicKey).toBytes(false).slice(1);
+  const stealthPublicKeyHash = keccak256(stealthPublicKey, "bytes").slice(12);
+  const stealthAddress = checksumAddress(`0x${toHex(stealthPublicKeyHash).slice(2)}`);
+
+  return {
+    stealthAddress,
+    ephemeralPublicKey,
+    viewTag,
+  };
+}
+
+export function checkStealthAddress(
+  expectedStealthAddress: Address,
+  ephemeralPublicKeyHex: Hex,
+  expectedViewTagHex: Hex,
+  viewingPrivateKey: Hex,
+  spendingPublicKeyHex: Hex,
+) {
+  const ephemeralPublicKey = secp256k1.Point.fromBytes(fromHex(ephemeralPublicKeyHex, { size: 33, to: "bytes" }));
+  const spendPublicKey = secp256k1.Point.fromBytes(fromHex(spendingPublicKeyHex, { size: 33, to: "bytes" }));
+
+  const sharedSecretPoint = ephemeralPublicKey.multiply(BigInt(viewingPrivateKey)).toAffine();
+  const sharedSecretX = sharedSecretPoint.x;
+  const sharedSecretXHashed = keccak256(toBytes(sharedSecretX), "bytes");
+
+  const viewTag = toHex(sharedSecretXHashed.slice(0, 1));
+
+  if (expectedViewTagHex !== viewTag) {
+    return false;
+  }
+
+  const sharedSecretXHashedPublicKey = secp256k1.Point.fromPrivateKey(sharedSecretXHashed);
+
+  const stealthPublicKey = spendPublicKey.add(sharedSecretXHashedPublicKey).toBytes(false).slice(1);
+  const stealthPublicKeyHash = keccak256(stealthPublicKey, "bytes").slice(12);
+  const stealthAddress = checksumAddress(`0x${toHex(stealthPublicKeyHash).slice(2)}`);
+
+  return expectedStealthAddress === stealthAddress;
+}
+
+export function computeStealthKey(
+  expectedStealthAddress: Address,
+  ephemeralPublicKeyHex: Hex,
+  viewingPrivateKey: Hex,
+  spendingPrivateKey: Hex,
+) {
+  const ephemeralPublicKey = secp256k1.Point.fromBytes(fromHex(ephemeralPublicKeyHex, { size: 33, to: "bytes" }));
+
+  const sharedSecretPoint = ephemeralPublicKey.multiply(BigInt(viewingPrivateKey)).toAffine();
+  const sharedSecretX = sharedSecretPoint.x;
+  const sharedSecretXHashed = keccak256(toBytes(sharedSecretX), "bytes");
+
+  const stealthPrivateKey =
+    BigInt(spendingPrivateKey) + (bytesToBigInt(sharedSecretXHashed) % secp256k1.Point.CURVE().n);
+  if (privateKeyToAddress(toHex(stealthPrivateKey)) !== expectedStealthAddress) {
+    throw new Error("Computed stealth private key does not match expected stealth address");
+  }
+
+  return toHex(stealthPrivateKey);
 }
